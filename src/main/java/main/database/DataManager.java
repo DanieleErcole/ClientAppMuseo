7e75350d3f9a472a9bcf54336a91fe58e3ad4c4d
DanieleErcole <78435419+DanieleErcole@users.noreply.github.com>
2021-03-30 16:32:50 +0200

    Aggiunta libreria per JSON, classe DataManager
    Libreria aggiunta: Jackson, prova di implementazione di un metodo per fare una richiesta http

--- src/main/java/main/database/DataManager.java
+++ src/main/java/main/database/DataManager.java
@@ -1,4 +1,37 @@
 package main.database;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataManager {
+
+    private final String hostname;
+    private final URL url; // request-script.php
+    private HttpURLConnection httpConnection;
+
+    public DataManager(String hostname, URL url) {
+        this.hostname = hostname;
+        this.url = url;
+    }
+
+    public <T> T requestData(Class<T> dataClass, HashMap<String, String> params) throws IOException {
+        StringBuilder requestUrl = new StringBuilder(url + "?");
+        for(Map.Entry e : params.entrySet())
+            requestUrl.append(e.getKey()).append("=").append(e.getValue());
+
+        InputStream stream;
+        httpConnection = (HttpURLConnection) url.openConnection();
+        httpConnection.setRequestProperty("accept", "application/json");
+        stream = httpConnection.getInputStream();
+
+        ObjectMapper mapper = new ObjectMapper();
+        return dataClass.cast(mapper.readValue(stream, dataClass));
+    }
+
 }
