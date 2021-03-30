e7d597b835b75d5cb74f829fac0773e9fa099d70
DanieleErcole <78435419+DanieleErcole@users.noreply.github.com>
2021-03-30 14:18:47 +0200

    aggiunta delle classi per la gestione dell'audio

--- src/main/java/main/audio/AudioManager.java
+++ src/main/java/main/audio/AudioManager.java
@@ -1,4 +1,44 @@
 package main.audio;
 
+import javax.sound.sampled.*;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Classe che gestisce la riproduzione dei suoni SOLO FILE .WAV
+ * @author Daniele Pelizzoni
+ */
+
 public class AudioManager {
+
+    private final Mixer mixer;
+    private AudioTrack currentTrack;
+
+    /**
+     * Costruttore degli oggetti di classe AudioManager
+     */
+    public AudioManager() {
+        Mixer.Info[] infos = AudioSystem.getMixerInfo();
+        mixer = AudioSystem.getMixer(infos[3]);
+    }
+
+    /**
+     * Metodo che fa partire la clip audio con il nome dato
+     * @param path -> nome della clip audio
+     * @param isLooped -> flag che identifica se l'audio deve essere riprodotto in loop
+     */
+    public void addTrack(String path, boolean isLooped) {
+        try {
+            DataLine.Info dataInfo = new DataLine.Info(Clip.class, null);
+            currentTrack = new AudioTrack((Clip) mixer.getLine(dataInfo), AudioSystem.getAudioInputStream(new URL(path)),.02f, isLooped, new URL(path));
+            //tracks.put(clipName, new AudioTrack((Clip) mixer.getLine(dataInfo), AudioSystem.getAudioInputStream(Assets.getSounds().get(clipName)),.02f, isLooped, clipName));
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public AudioTrack getCurrentTrack() {
+        return currentTrack;
+    }
+
 }
--- src/main/java/main/audio/AudioTrack.java
+++ src/main/java/main/audio/AudioTrack.java
@@ -1,4 +1,103 @@
 package main.audio;
 
+import javax.sound.sampled.*;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Classe che rappresenta una generica traccia audio
+ * @author Daniele Pelizzoni
+ */
+
 public class AudioTrack {
+
+    private final URL clipURL;
+    private final Clip thisClip;
+    private AudioInputStream thisStream;
+
+    private float volume;
+    private long currentFrame;
+    private final boolean isLooped;
+
+    /**
+     * Costruttore degli oggetti di classe AudioTrack
+     * @param thisClip -> clip audio da riprodurre
+     * @param thisStream -> stream audio che riproduce la traccia
+     * @param volume -> livello di volume
+     * @param isLooped -> flag che indica se la traccia deve essere riprodotta in loop
+     */
+    public AudioTrack(Clip thisClip, AudioInputStream thisStream, float volume, boolean isLooped, URL url) {
+        this.thisClip = thisClip;
+        this.thisStream = thisStream;
+        this.volume = volume;
+        this.isLooped = isLooped;
+        this.clipURL = url;
+        try {
+            this.thisClip.open(thisStream);
+            //Volume
+            FloatControl gainControl = (FloatControl) this.thisClip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(20f * (float) Math.log10(this.volume));
+        } catch (LineUnavailableException | IOException e) {
+            e.printStackTrace();
+        }
+        currentFrame = 0L;
+    }
+
+    /**
+     * Metodo che inizia la riproduzione della traccia
+     */
+    public void start() {
+        assert thisStream != null : "audioStream not initialized";
+        if(isLooped)
+            thisClip.loop(Clip.LOOP_CONTINUOUSLY);
+        thisClip.start();
+    }
+
+    /**
+     * Metodo che mette in pausa la traccia
+     */
+    public void pause() {
+        currentFrame = thisClip.getMicrosecondPosition();
+        thisClip.stop();
+    }
+
+    /**
+     * Metodo che riprende la riproduzione della traccia
+     */
+    public void resume() {
+        if(!thisClip.isOpen() || !thisClip.isActive()) {
+            thisClip.close();
+            try {
+                thisStream = AudioSystem.getAudioInputStream(clipURL);
+                thisClip.open(thisStream);
+                if(isLooped)
+                    thisClip.loop(Clip.LOOP_CONTINUOUSLY);
+            } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
+                e.printStackTrace();
+            }
+            thisClip.setMicrosecondPosition(currentFrame);
+            thisClip.start();
+        }
+    }
+
+    /**
+     * Metodo che ferma la riproduzione della traccia
+     */
+    public void stop() {
+        if(thisClip.isRunning() || thisClip.isActive() || thisClip.isOpen()) {
+            thisClip.stop();
+            thisClip.close();
+            try {
+                thisStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //Getter
+    public float getVolume() {
+        return volume;
+    }
+
 }
